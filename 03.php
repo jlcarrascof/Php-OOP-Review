c9159f3f8d7f0c3dda1bd7014ab36d4f44b7e607
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-09-15 21:18:46 -0400

    Add var_dump for employee object

--- 03.php
+++ 03.php
@@ -1,2 +1,14 @@
 <?php
 include 'includes/header.php';
+
+
+// Instance of the Employee class
+class Employee {
+
+}
+
+$employee = new Employee();
+
+echo '<pre>';
+var_dump($employee);
+echo '</pre>';
