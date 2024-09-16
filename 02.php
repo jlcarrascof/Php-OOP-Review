c0a8a9810622b1af7555959cefc7184f4fda3280
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-09-15 21:16:20 -0400

    Add var_dump for multiple employee objects

--- 02.php
+++ 02.php
@@ -7,7 +7,17 @@ class Employee {
 }
 
 $employee = new Employee();
+$employee2 = new Employee();
+$employee3 = new Employee();
 
 echo '<pre>';
 var_dump($employee);
+echo '</pre>';
+
+echo '<pre>';
+var_dump($employee2);
+echo '</pre>';
+
+echo '<pre>';
+var_dump($employee3);
 echo '</pre>';
