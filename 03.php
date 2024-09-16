4dd7b2a73a867c1562de3a66f9de8063493ea8d1
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-09-15 22:24:31 -0400

    Refactor Employee class attributes

--- 03.php
+++ 03.php
@@ -4,7 +4,11 @@ include 'includes/header.php';
 
 // Attributes of the Employee class
 class Employee {
-
+    public $name;
+    public $lastname;
+    public $department;
+    public $email;
+    public $code;
 }
 
 $employee = new Employee();
