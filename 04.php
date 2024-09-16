270e7ce93bde5942f35456531bfd4a711195ffab
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-09-16 12:15:37 -0400

    Refactor Employee class attributes and add name and lastname properties to Employee object

--- 04.php
+++ 04.php
@@ -1,2 +1,11 @@
 <?php
 include 'includes/header.php';
+
+// Attributes of the Employee class
+class Employee {
+    public $name;
+    public $lastname;
+    public $department;
+    public $email;
+    public $code;
+}
