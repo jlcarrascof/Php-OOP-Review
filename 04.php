b8234b659eebeacd1c980fe18a0a5c0009a0a289
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-09-16 13:13:03 -0400

    Refactor Employee class constructor to accept additional parameters for name, lastname, department, email, and code. Initialize the corresponding properties with the provided values.

--- 04.php
+++ 04.php
@@ -9,13 +9,16 @@ class Employee {
     public $email;
     public $code;
 
-    public function __construct() {
-        echo 'Hello World!';
+    public function __construct($name, $lastname, $department, $email, $code) {
+        $this->name = $name;
+        $this->lastname = $lastname;
+        $this->department = $department;
+        $this->email = $email;
+        $this->code = $code;
     }
 }
 
-$employee = new Employee();
-
+$employee = new Employee('John', 'Doe', 'IT', 'jdoe@me.com', '1234');
 
 echo '<pre>';
 var_dump($employee);
