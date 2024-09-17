6c8244147ecad6df75facc5d2c570d554a55fb77
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-09-17 18:10:09 -0400

    Refactor Employee class to change strings by number values

--- 06.php
+++ 06.php
@@ -9,13 +9,11 @@ class Employee {
         public $department,
         public $email,
         public $code,
-    ) {
-
-    }
+    ) {}
 }
 
-$john = new Employee('John', 'Doe', 'IT', 'jdoe@me.com', '006');
-$tatiana = new Employee('Tatiana', 'Doe', 'MKT', 'tatiana@me.com', '007');
+$john = new Employee('John', 'Doe', 'IT', 'jdoe@me.com', 006);
+$tatiana = new Employee('Tatiana', 'Doe', 'MKT', 'tatiana@me.com', 007);
 
 
 echo '<pre>';
