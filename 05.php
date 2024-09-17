25ccc978a76e03c752dd02f568b909eb8bb4cbac
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-09-17 13:38:30 -0400

    Refactor Employee class constructor to accept additional parameters for name, lastname, department, email, and code. Initialize the corresponding properties with the provided values and add type hinting. Create instances of Employee class with sample data for testing.

--- 05.php
+++ 05.php
@@ -1,2 +1,31 @@
 <?php
 include 'includes/header.php';
+
+// Type hinting
+class Employee {
+    public $name;
+    public $lastname;
+    public $department;
+    public $email;
+    public $code;
+
+    public function __construct($name, $lastname, $department, $email, $code) {
+        $this->name = $name;
+        $this->lastname = $lastname;
+        $this->department = $department;
+        $this->email = $email;
+        $this->code = $code;
+    }
+}
+
+$john = new Employee('John', 'Doe', 'IT', 'jdoe@me.com', '006');
+$tatiana = new Employee('Tatiana', 'Doe', 'MKT', 'tatiana@me.com', '007');
+
+
+echo '<pre>';
+var_dump($john);
+echo '</pre>';
+
+echo '<pre>';
+var_dump($tatiana);
+echo '</pre>';
