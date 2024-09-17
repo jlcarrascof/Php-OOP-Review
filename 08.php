3768598a062f9c9d4d451a1249b2990c155641bb
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-09-17 18:52:28 -0400

    Refactor Employee class constructor to add new parameters and initialize properties accordingly

--- 08.php
+++ 08.php
@@ -1,2 +1,26 @@
 <?php
 include 'includes/header.php';
+
+// Methods
+class Employee {
+    public $name;
+    public $lastname;
+    public $department;
+    public $email;
+    public $code;
+
+    public function __construct($name, $lastname, $department, $email, $code) {
+        $this->name = $name;
+        $this->lastname = $lastname;
+        $this->department = $department;
+        $this->email = $email;
+        $this->code = $code;
+    }
+
+}
+
+$john = new Employee('John', 'Doe', 'IT', 'jdoe@me.com', 006);
+
+echo '<pre>';
+var_dump($john);
+echo '</pre>';
