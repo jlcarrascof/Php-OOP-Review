d57e34953f5c1b749cc6af5b6bf0a0facbe48a2d
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-09-20 19:51:39 -0400

    Refactor Employee class to add properties for lastname, department, and code

--- 10.php
+++ 10.php
@@ -1,2 +1,12 @@
 <?php
 include 'includes/header.php';
+
+// Inheritance
+class Employee {
+    protected $name;
+    protected $lastname;
+    protected $email;
+    protected $phone;
+    protected $code;
+    protected $department;
+}
