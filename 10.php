f28f4e9f5de45c5ff15b93dc1176656dd37d4ad9
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-09-20 20:50:17 -0400

    Refactor Employee class to add constructor with parameters for name, lastname, email, phone, code, and department properties

--- 10.php
+++ 10.php
@@ -9,6 +9,15 @@ class Employee {
     protected $phone;
     protected $code;
     protected $department;
+
+    public function __construct($name, $lastname, $email, $phone, $code, $department) {
+        $this->name = $name;
+        $this->lastname = $lastname;
+        $this->email = $email;
+        $this->phone = $phone;
+        $this->code = $code;
+        $this->department = $department;
+    }
 }
 
 class Provider {
