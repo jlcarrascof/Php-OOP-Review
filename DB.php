d1901ed5183293ae752ceb5456d57fef5db4cda8
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-09-24 18:45:30 -0400

    Refactor code to add save method in DB class

--- DB.php
+++ DB.php
@@ -4,8 +4,14 @@ class DB {
 
     protected $name;
 
-    public function __construct($name) {
+    public function __construct($name)
+    {
         $this->name = $name;
+    }
+
+    public function save()
+    {
+        echo "Saving .... " . $this->name;
     }
 
 }
